84450b5aa69a2a8e2b6914e4c87c20a67c5c7cd3
Marcelo Sercheli <msercheli@gmail.com>
2023-05-04 09:54:49 -0400

    Add rez files

---
+++ package.py
@@ -0,0 +1,42 @@
+name = 'spdlog'
+
+version = '1.9.2'
+
+authors = [
+    'Gabi Melman',
+]
+
+description = '''Fast header-only C++ log'''
+
+with scope('config') as c:
+    # Determine location to release: internal (int) vs external (ext)
+    # NOTE: Modify this variable to reflect the current package situation
+    release_as = 'ext'
+
+    # The 'c' variable here is actually rezconfig.py
+    # 'release_packages_path' is a variable defined inside rezconfig.py
+
+    import os
+    if release_as == 'int':
+        c.release_packages_path = os.environ['SSE_REZ_REPO_RELEASE_INT']
+    elif release_as == 'ext':
+        c.release_packages_path = os.environ['SSE_REZ_REPO_RELEASE_EXT']
+
+requires = [
+]
+
+private_build_requires = [
+]
+
+variants = [
+    ['platform-linux', 'arch-x86_64', 'os-centos-7'],
+]
+
+def pre_build_commands():
+    command("source /opt/rh/devtoolset-6/enable")
+
+def commands():
+    env.REZ_SPDLOG_ROOT = '{root}'
+
+uuid = 'repository.spdlog'
+
